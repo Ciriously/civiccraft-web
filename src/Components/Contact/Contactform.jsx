decf2596a1ddaae8d4e3fa8d341f095a99192406
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2024-02-25 22:20:16 +0530

    fixed on click issue in forms

--- src/Components/Contact/Contactform.jsx
+++ src/Components/Contact/Contactform.jsx
@@ -18,9 +18,17 @@ const ContactForm = () => {
 
     // Function to handle social media selection
     const handleSocialClick = (social) => {
-        setHowHeard((prevHowHeard) => prevHowHeard + (prevHowHeard ? `, ${social}` : social));
+        setHowHeard((prevHowHeard) => {
+            // Check if prevHowHeard already contains the selected social media
+            if (prevHowHeard.includes(social)) {
+                // If it does, return the previous value without appending
+                return prevHowHeard;
+            } else {
+                // If it doesn't, append the selected social media with a comma
+                return prevHowHeard ? `${prevHowHeard}, ${social}` : social;
+            }
+        });
     };
-
     // Function to handle attachment selection
     const handleAttachmentChange = (file) => {
         setAttachment(file);
@@ -39,11 +47,11 @@ const ContactForm = () => {
     };
 
     return (
-        <form className="bg-white font-gordita-medium rounded-t-lg px-8 pt-40 pb-8 mb-4">
+        <form className="bg-white font-gordita-medium rounded-t-lg px-8 pt-10 md:pt-40 pb-8 mb-4">
             {/* Full Name */}
-            <div className="mb-8">
+            <div className="mb-4">
                 <input
-                    className="shadow-none appearance mb-4 border-b border-gray-300 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 text-lg"
+                    className="shadow-none appearance mb-2 md:mb-4 border-b border-gray-300 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 text-lg"
                     id="fullName"
                     type="text"
                     placeholder="Full Name"
@@ -52,9 +60,9 @@ const ContactForm = () => {
                 />
             </div>
             {/* Phone Number */}
-            <div className="mb-8">
+            <div className="mb-4">
                 <input
-                    className="shadow-none appearance-none mb-4 border-b border-gray-300 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 text-lg"
+                    className="shadow-none appearance-none mb-2 md:mb-4 border-b border-gray-300 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 text-lg"
                     id="phoneNumber"
                     type="tel"
                     placeholder="Phone Number"
@@ -63,9 +71,9 @@ const ContactForm = () => {
                 />
             </div>
             {/* Email */}
-            <div className="mb-8">
+            <div className="mb-4">
                 <input
-                    className="shadow-none appearance-none mb-4 border-b border-gray-300 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 text-lg"
+                    className="shadow-none appearance-none mb-2 md:mb-4 border-b border-gray-300 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 text-lg"
                     id="email"
                     type="email"
                     placeholder="Email ID"
@@ -74,9 +82,9 @@ const ContactForm = () => {
                 />
             </div>
             {/* How did you hear about us? */}
-            <div className="mb-8">
+            <div className="mb-4">
                 <input
-                    className="shadow-none appearance-none mb-2 border-b border-gray-300 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 text-lg"
+                    className="shadow-none appearance-none mb-2 md:mb-4 border-b border-gray-300 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 text-lg"
                     id="howHeard"
                     type="text"
                     placeholder="How did you hear about us?"
@@ -85,53 +93,59 @@ const ContactForm = () => {
                 />
             </div>
             {/* Social Media Selection */}
-            <div className="flex space-x-5 mb-8 ml-3 font-gordita-medium">
+            <div className="flex flex-wrap mb-4 ml-3 font-gordita-medium">
                 <button
+                    type="button"
                     onClick={() => handleSocialClick('Facebook')}
-                    className="flex items-center space-x-2 px-2 py- rounded-md border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out focus:outline-none"
+                    className="flex items-center space-x-2 px-2 py- rounded-md border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out focus:outline-none mb-2 mr-2"
                 >
                     <img src={facebookLogo} alt="Facebook Logo" className="h-4 w-4" />
                     <span>Facebook</span>
                 </button>
                 <button
+                    type="button"
                     onClick={() => handleSocialClick('Twitter')}
-                    className="flex items-center space-x-2 px-2 py-1 rounded-md border border-light-blue-500 text-light-blue-500 hover:bg-light-blue-500 hover:text-gray-400 transition duration-300 ease-in-out focus:outline-none"
+                    className="flex items-center space-x-2 px-2 py-1 rounded-md border border-light-blue-500 text-light-blue-500 hover:bg-light-blue-500 hover:text-gray-400 transition duration-300 ease-in-out focus:outline-none mb-2 mr-2"
                 >
                     <img src={twitterLogo} alt="Twitter Logo" className="h-4 w-4" />
                     <span>Twitter</span>
                 </button>
                 <button
+                    type="button"
                     onClick={() => handleSocialClick('Instagram')}
-                    className="flex items-center space-x-2 px-2 py-1 rounded-md border border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white transition duration-300 ease-in-out focus:outline-none"
+                    className="flex items-center space-x-2 px-2 py-1 rounded-md border border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white transition duration-300 ease-in-out focus:outline-none mb-2 mr-2"
                 >
                     <img src={instagramLogo} alt="Instagram Logo" className="h-4 w-4" />
                     <span>Instagram</span>
                 </button>
                 <button
+                    type="button"
                     onClick={() => handleSocialClick('LinkedIn')}
-                    className="flex items-center space-x-2 px-2 py-1 rounded-md border border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white transition duration-300 ease-in-out focus:outline-none"
+                    className="flex items-center space-x-2 px-2 py-1 rounded-md border border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white transition duration-300 ease-in-out focus:outline-none mb-2 mr-2"
                 >
                     <img src={linkedinLogo} alt="LinkedIn Logo" className="h-4 w-4" />
                     <span>LinkedIn</span>
                 </button>
                 <button
+                    type="button"
                     onClick={() => handleSocialClick('Website')}
-                    className="flex items-center space-x-2 px-2 py-1 rounded-md border border-purple-500 text-purple-500 hover:bg-purple-500 hover:text-white transition duration-300 ease-in-out focus:outline-none"
+                    className="flex items-center space-x-2 px-2 py-1 rounded-md border border-purple-500 text-purple-500 hover:bg-purple-500 hover:text-white transition duration-300 ease-in-out focus:outline-none mb-2 mr-2"
                 >
                     <img src={websiteLogo} alt="Website Logo" className="h-4 w-4" />
                     <span>Others</span>
                 </button>
             </div>
+
             {/* Project Description */}
-            <div className="mb-8">
+            <div className="mb-4">
                 <textarea
-                    className="shadow-none appearance-none mb-4 border-b border-gray-300 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 text-lg"
+                    className="shadow-none appearance-none mb-2 md:mb-4 border-b border-gray-300 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 text-lg"
                     id="projectDescription"
                     placeholder="Project Description"
                 />
             </div>
             {/* Attachment */}
-            <div className="mb-8 relative">
+            <div className="mb-4 relative">
                 <input
                     type="file"
                     className="hidden"
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -7,11 +7,11 @@ const ContactUs = () => {
     <div>
       <div className="bg-white"><Navbar /></div>
 
-      <div className="flex h-screen bg-white">
+      <div className="flex flex-col-reverse md:flex-row bg-white">
 
-        <div className="w-1/2  font-gordita-medium bg-gradient-to-br from-indigo-500 via-indigo-300 to-indigo-100 text-white text-lg p-8 flex flex-col  justify-center">
+        <div className="md:w-1/2 font-gordita-medium bg-gradient-to-br from-indigo-500 via-indigo-300 to-indigo-100 text-white text-lg p-8 flex flex-col justify-center hidden md:flex">
           <div className="mb-8">
-            <h2 className="text-5xl font-gordita-bold  mb-4 text-center">Get in Touch</h2>
+            <h2 className="text-5xl font-gordita-bold mb-4 text-center">Get in Touch</h2>
             <p className="text-center">123 Main Street</p>
             <p className="text-center">City, State, ZIP</p>
           </div>
@@ -25,26 +25,10 @@ const ContactUs = () => {
           </div>
           <div>
             <h3 className="text-xl font-bold mb-2 text-center">Connect with Us</h3>
-            {/* <div className="flex items-center space-x-4">
-            <a href="#" className="text-white">
-              <img src={facebookLogo} alt="Facebook Logo" className="h-6 w-6" />
-            </a>
-            <a href="#" className="text-white">
-              <img src={twitterLogo} alt="Twitter Logo" className="h-6 w-6" />
-            </a>
-            <a href="#" className="text-white">
-              <img src={instagramLogo} alt="Instagram Logo" className="h-6 w-6" />
-            </a>
-            <a href="#" className="text-white">
-              <img src={linkedin} alt="LinkedIn Logo" className="h-6 w-6" />
-            </a>
-            <a href="#" className="text-white">
-              <img src={websiteLogo} alt="Website Logo" className="h-6 w-6" />
-            </a>
-          </div> */}
+            {/* Social media links */}
           </div>
         </div>
-        <div className="w-1/2 p-16">
+        <div className="w-full md:w-1/2 p-16">
           <ContactForm />
         </div>
       </div>
