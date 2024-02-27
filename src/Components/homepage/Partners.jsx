ff9a1d3e8c634aa6f5852d57bb7fd0f903215eb0
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2024-02-27 17:26:29 +0530

    added partners

--- src/Components/homepage/Partners.jsx
+++ src/Components/homepage/Partners.jsx
@@ -5,22 +5,25 @@ import Maha from '../../assets/partners/Maha.png';
 import Jharkand from '../../assets/partners/Jharkand.png';
 import ISRO from '../../assets/partners/ISRO.png';
 import UP from '../../assets/partners/UP.png';
+import BG from '../../assets/partners/bg.jpg';
 
 const OurPartners = () => {
   return (
-    <div className="p-8 mx-auto max-w-7xl">
-      <h1 className="text-4xl font-gordita-bold mb-8 text-left">Our Partners</h1>
-      <div className="bg-[#D9D9D9]  font-gordita-bold text-xl p-4 rounded w-72 mb-24 mx-auto text-center">
-        <p>Government Collaborations</p>
-      </div>
+    <div className="bg-cover bg-center" style={{ backgroundImage: `url(${BG})` }}>
+      <div className="p-8 mx-auto max-w-7xl">
+        <h1 className="text-4xl text-white font-gordita-bold mb-8 text-left">Our Partners</h1>
+        <div className=" text-white font-gordita-bold text-2xl p-4 mb-24 text-left">
+          <p>Government Collaborations</p>
+        </div>
 
-      <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
-        <img className="w-44 h-auto" src={Maha} alt="Partner 3" />
-        <img className="w-44 h-auto" src={UP} alt="Partner 6" />
-        <img className="w-44 h-auto" src={ISRO} alt="Partner 5" />
-        <img className="w-44 h-auto" src={Uk} alt="Partner 2" />
-        <img className="w-44 h-auto" src={Jharkand} alt="Partner 4" />
-        <img className="w-44 h-auto" src={tele} alt="Partner 1" />
+        <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
+          <img className="w-44 h-auto" src={Maha} alt="Partner 3" />
+          <img className="w-44 h-auto" src={UP} alt="Partner 6" />
+          <img className="w-44 h-auto" src={ISRO} alt="Partner 5" />
+          <img className="w-44 h-auto" src={Uk} alt="Partner 2" />
+          <img className="w-44 h-auto" src={Jharkand} alt="Partner 4" />
+          <img className="w-44 h-auto" src={tele} alt="Partner 1" />
+        </div>
       </div>
     </div>
   );
